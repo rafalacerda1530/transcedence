f393ec7740ef0f270859900acc55948884763983
Luis Paulo Santos <lp_santos29@outlook.com>
2023-11-24 15:13:26 -0300

    Add unique id for friendship

--- back/prisma/migrations/20231124180918_friendship/migration.sql
+++ back/prisma/migrations/20231124180918_friendship/migration.sql
@@ -1,12 +1,16 @@
 -- CreateTable
 CREATE TABLE "Friendship" (
+    "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "followedById" INTEGER NOT NULL,
     "followingId" INTEGER NOT NULL,
 
-    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("followingId","followedById")
+    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Friendship_followingId_followedById_key" ON "Friendship"("followingId", "followedById");
 
 -- AddForeignKey
 ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_followedById_fkey" FOREIGN KEY ("followedById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
