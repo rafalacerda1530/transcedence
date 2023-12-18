12d61743a8c1b9ed22a5cb00a3af8e735c1279fe
Matomomitsu <matomomitsu@gmail.com>
2023-12-18 18:29:03 -0300

    working paddles

--- front/src/Pages/Game/Game.tsx
+++ front/src/Pages/Game/Game.tsx
@@ -1,89 +1,84 @@
 import React, { useContext, useEffect, useMemo, useState } from "react";
 import { GameContext } from "../../context/GameContext";
 import { useRefreshToken } from "../../hooks/useRefreshToken";
+import Paddle from "../../components/paddle";
 
 export const Game = () => {
-  const socket = useContext(GameContext);
-	const queryParams = useMemo(() => {
-		return new URLSearchParams(window.location.search);
-	}, []);
-	const [roomId, setRoomId] = useState("");
-  const refreshToken = useRefreshToken();
+    const socket = useContext(GameContext);
+    const queryParams = useMemo(() => {
+        return new URLSearchParams(window.location.search);
+    }, []);
+    const [roomId, setRoomId] = useState("");
+    const refreshToken = useRefreshToken();
 
+    useEffect(() => {
+        const newroomId = queryParams.get("roomId");
+        if (newroomId) setRoomId(newroomId);
+        else window.location.href = "http://localhost:3000/Queue";
+    }, [queryParams]);
 
-	useEffect(() => {
-		const newroomId = queryParams.get("roomId");
-		if (newroomId)
-			setRoomId(newroomId);
-		else
-			window.location.href = 'http://localhost:3000/Queue';
-	}, [queryParams]);
+    const connectSocket = () => {
+        if (roomId) {
+            socket.connect();
+            socket.on("connect", () => {
+                console.log("Conectado ao socket");
+                console.log(roomId);
+                socket.emit("joinRoom", { roomId: roomId });
+                socket.emit("moveUp", { roomId: roomId });
+                socket.emit("moveDown", { roomId: roomId });
+            });
 
-	const connectSocket = () => {
-		if (roomId){
-			socket.connect();
-			socket.on("connect", () => {
-				console.log("Conectado ao socket");
-				console.log(roomId);
-				socket.emit("joinRoom", { roomId: roomId });
-				socket.emit("moveUp", { roomId: roomId });
-				socket.emit("moveDown", { roomId: roomId });
-			});
+            socket.on("jwt_error", async (error) => {
+                console.log(`Connection failed due to ${error.message}`);
+                console.log("Tentando Reautenticar");
+                disconnectSocket();
+                try {
+                    await refreshToken();
+                } catch (error) {
+                    console.log(error);
+                    window.location.href = "http://localhost:3000/login";
+                }
+                connectSocket();
+            });
 
-			socket.on("jwt_error", async (error) => {
-				console.log(`Connection failed due to ${error.message}`);
-				console.log("Tentando Reautenticar");
-				disconnectSocket();
-				try {
-					await refreshToken();
-				} catch (error) {
-					console.log(error);
-					window.location.href = 'http://localhost:3000/login';
-				}
-				connectSocket();
-			});
+            socket.on("moveUp", () => {
+                console.log("Opponent Moved Up");
+            });
 
-			socket.on("moveUp", () => {
-				console.log("Opponent Moved Up");
-			});
+            socket.on("moveDown", () => {
+                console.log("Opponent Moved Down");
+            });
+        }
+    };
 
-			socket.on("moveDown", () => {
-				console.log("Opponent Moved Down");
-			});
-		}
-	};
+    const disconnectSocket = () => {
+        if (roomId) {
+            socket.off("connect");
+            socket.off("jwt_error");
+            socket.off("moveUp");
+            socket.off("moveDown");
+            socket.disconnect();
+        }
+    };
 
-	const disconnectSocket = () => {
-		if (roomId){
-			socket.off('connect');
-			socket.off('jwt_error');
-			socket.off("moveUp");
-			socket.off("moveDown");
-			socket.disconnect();
-		}
-	};
-
-	useEffect(() => {
-		connectSocket();
-		return () => {
-			console.log("Desconectando do socket");
-			disconnectSocket();
-		}
-	}, [socket, roomId]);
+    useEffect(() => {
+        connectSocket();
+        return () => {
+            console.log("Desconectando do socket");
+            disconnectSocket();
+        };
+    }, [socket, roomId]);
 
     return (
-    <>
-      <div className="h-screen bg-gradient-to-b from-purple-700 via-purple-400 to-purple-700 flex items-center justify-center relative">
-                <div className="absolute top-0 left-0 right-0 w-32 h-32 mx-auto mt-52 mb-0 bg-white rounded-full animate-bounce ">
-                {/*bola de ping pong */}
-                </div>
-                <h1 className="text-4xl font-bold text-center text-white mb-4">
-                    SEARCHING PLAYERS
-                </h1>
-      </div>
-    </>
-  );
-}
-function refreshToken() {
-  throw new Error("Function not implemented.");
-}
+		<>
+		<div className="h-screen bg-gradient-to-b from-purple-700 via-purple-400 to-purple-700 flex relative">
+			<div style={{position: 'absolute', left: '15%'}}>
+				<Paddle initialPosition={window.innerHeight * 4 / 10 - 10} />
+			</div>
+			<div style={{position: 'absolute', right: '15%'}}>
+				<Paddle initialPosition={window.innerHeight * 4 / 10 - 10} />
+			</div>
+		</div>
+	</>
+    );
+};
---
+++ front/src/components/paddle.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef, useState } from "react";
+
+interface PaddleProps {
+ initialPosition: number;
+}
+
+function Paddle({ initialPosition }: PaddleProps) {
+ const [position, setPosition] = useState(initialPosition);
+ const positionRef = useRef(position);
+ const paddleRef = useRef<HTMLDivElement>(null);
+
+ const handleKeyDown = (e: KeyboardEvent) => {
+    if (paddleRef.current) {
+        const paddleHeight = paddleRef.current.offsetHeight;
+
+		console.log(paddleHeight);
+		console.log(window.innerHeight);
+
+        if ((e.key === "w" || e.key === "W") && positionRef.current - window.innerHeight / 100 >= (window.innerHeight / 10)) {
+            setPosition((prevPosition) => prevPosition - window.innerHeight / 100);
+        } else if ((e.key === "s" || e.key === "S") && positionRef.current + window.innerHeight / 100 <= (window.innerHeight * 9 / 10) - paddleHeight) {
+            setPosition((prevPosition) => prevPosition + window.innerHeight / 100);
+        }
+    }
+ };
+
+ useEffect(() => {
+    positionRef.current = position;
+ }, [position]);
+
+ useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+    };
+ }, []);
+
+ return (
+    <div
+        ref={paddleRef}
+        className="absolute w-5 h-10 bg-white"
+        style={{ top: `${position}px` }}
+    />
+ );
+}
+
+export default Paddle;
